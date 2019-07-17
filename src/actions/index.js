bf39827fe6afa460b1fadf2719041fafa6287b1b
Michelle Scott <scottmm374@gmail.com>
2019-07-16 20:11:28 -0400

    added action to request data from axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,27 @@
-// we'll need axios
+import axios from 'axios';
+
+export const GET_DATA_START = 'GET_DATA_START';
+export const GET_DATA_SUCCESS = 'GET_DATA_SUCCESS';
+export const GET_DATA_FAILED = 'GET_DATA_FAILED';
+
+
+
+
+
+
+export function getData() {
+    return (dispatch) => {
+        dispatch({ type: GET_DATA_START })
+
+        axios.get(`https://swapi.co/api/people/`)
+            .then((res) => {
+                dispatch({ type: GET_DATA_SUCCESS, payload: res.data })
+            })
+            .catch((error) => {
+                dispatch({ type: GET_DATA_FAILED, payload: error.response.data})
+            })
+    }
+}
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
